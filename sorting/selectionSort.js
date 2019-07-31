2aa5fc7180cdbaf11aa0002b9b7470e9424c63e3
Allen Hsieh <allenhsiehhr@gmail.com>
2019-07-31 12:42:41 -0700

    selectionSort completed

---
+++ sorting/selectionSort.js
@@ -0,0 +1,18 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIdx = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIdx]) {
+        minIdx = j;
+      }
+    }
+    if (minIdx !== i) {
+      let temp = arr[i];
+      arr[i] = arr[minIdx];
+      arr[minIdx] = temp;
+    }
+  }
+  return arr;
+}
+
+console.log(selectionSort([2, 1, 3]));
